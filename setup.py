838f020086d02a93969c81f415e8da3aeb3e1c0a
Jeff Balogh <me@jeffbalogh.org>
2009-05-14 21:36:26 -0700

    version 0.0.3, now with more awesome

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='poboy',
-      version='0.0.2',
+      version='0.0.3',
       author='Jeff Balogh',
       author_email='jbalogh@mozilla.com',
       url='http://github.com/jbalogh/poboy',
